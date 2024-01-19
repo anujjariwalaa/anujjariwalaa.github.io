cb9ef894b01ae3972d7cacf411b208f4716a53c3
spytrac <anujjariwala@arizona.edu>
2024-01-19 13:40:26 -0700

    changes done for now

--- js/general-script.js
+++ js/general-script.js
@@ -5,7 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	// Function to update the icon and image classes
 	function updateTheme() {
-		icon.transition = "all 1s";
 		if (document.body.classList.contains("light-theme")) {
 			icon.src = "images/moon.svg";
 			icon.classList.replace("invert-color-dark", "invert-color-light");
